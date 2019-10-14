6eb827b659862ade27e330b17974a0231a635bfb
aleesha-wood <spectrat@spectratart.com>
2019-10-14 15:43:11 -0400

    reset

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -6,73 +6,28 @@ export default function SearchForm() {
   const [searchQ, setSearchQ] = useState("");
   const [resultList, setResultList] = useState("");
   const [resultNames, setResultNames] = useState("");
-  useEffect(() => {
-    const getList = () => {
-      axios
-        .get(`https://rickandmortyapi.com/api/character/?name=${searchQ}`)
-        .then(response => {
-          console.log(response.data.results);
-          setResultList(response.data.results);
-        })
-        .catch(error => {
-          console.log("Error Message: ", error);
-        });
-    };
-    getList(); 
-    }, []);
-    //TODO ...add another component for rendering the search results??
-
   
   const handleChange = e => {
-    setSearchQ(e.target.value)
+    setSearchQ(e.target.value);
   };
   
   const handleSubmit = e => {
     e.preventDefault();
-    
-    
-    // axios
-    //   .get(`https://rickandmortyapi.com/api/character/?name=${searchQ}`)
-    //   .then(response => {
-    //     setResultList(response.data.results[3].name);
-    //     console.log(resultList);
 
-    //     function getNames(array) {
-    //       array.foreach(item) {
-    //         return item.name;
-    //       }
-    //     }
-        
-        //console.log(resultList);
-        //console.log("results: ", resultList);
-        //console.log(resultList[0].name);
-      //  setResultNames(resultList.forEach(function(thing) {
-      //   console.log("names are: ", thing.name);
-      //  }));
-
-       //console.log("Names are: ", resultNames);
-        
-
-
-        
-        // function getNames() {
-        //   resultList.foreach((result) => {
-        //     console.log(result.name);
-        //   });
-        // }
-        // getNames();
-        // const listNames = resultList.forEach(function(item) {
-        //   //console.log(item.name);
-        //   const list = item.name;
-        //   return list;
-        // });
-        //setResultNames(listNames);
-        //console.log("Here are the names: ", resultNames);
-        
-      // })
-      // .catch(error => {
-      //   console.log("Error Message: ", error);
-      // });
+    axios
+      .get(`https://rickandmortyapi.com/api/character/?name=${searchQ}`)
+      .then(response => {
+        console.log(response.data.results);
+      })
+      .then(response => {
+        setResultList("these are the results");
+        console.log("result list: ", resultList);
+      })
+      
+      .catch(error => {
+        console.log("Error Message: ", error);
+      });
+    //*Can log results in console but attempts of displaying them on the page have been unsuccessful... 
   };
 
   return (
