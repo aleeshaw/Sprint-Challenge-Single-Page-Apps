0b55d9d9671ceaa562dafbde86157e6533e96327
aleesha-wood <spectrat@spectratart.com>
2019-10-13 22:10:13 -0400

    added Welcome Page as home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
-import Header from "./components/Header.js";
+//import Navigation from "./components/Navigation.js";
+import { Route } from 'react-router-dom';
+import WelcomePage from "./components/WelcomePage.js";
+import CharacterList from "./components/CharacterList.js";
+//import Character from "./components/Character.js";
+import SearchForm from "./components/SearchForm.js";
 
 
 export default function App() {
   return (
     <main>
-      <Header />
+      {/* <Navigation /> */}
+      <Route exact path="/" component={WelcomePage} />
+      <Route exact path="/characters" component={CharacterList}/>
+      {/* <Route path="/characters/:id" component={Character} /> Put this iin the CharacterList component */}
+      <Route path="/search" component={SearchForm}/> {/*Put this in the CharacterList Page too*/}
     </main>
   );
 }
