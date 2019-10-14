348497459b5198a23eebe80ddee8fdc39c5c8d00
aleesha-wood <spectrat@spectratart.com>
2019-10-13 22:19:29 -0400

    added character page api call

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,11 +1,22 @@
 import React, { useEffect, useState } from "react";
+import Axios from "axios";
 
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
-
+  ///// TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
+    const getCharacters = () => {
+      Axios
+        .get("https://rickandmortyapi.com/api/character/")
+        .then(response => {
+          console.log(response.data.response);
+        })
+        .catch(error => {
+          console.log("Error Message: ", error);
+        });
+    };
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
